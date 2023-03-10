f365f307975ee5e9c23cc3c16922653e1a0b41b1
Javier Gracia <javiergracia@MacBook-Pro-de-Javier.local>
2023-03-10 21:50:54 +0100

    se muestra el texto al apretar el boton en el log.d

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,21 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView texto_mostrado;
+    private EditText texto_introducido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //texto_mostrado = (TextView) findViewById(R.id.solucion);
+        texto_mostrado = (TextView) findViewById(R.id.textView2);
+        texto_introducido = (EditText) findViewById(R.id.editTextTextPersonName);
         //texto_mostrado.setText("HOLA SOY JAVI");
     }
 
     public void boton_pulsado(View quien){
-       Log.d("prueba", "EL BOTON SE HA PULSADO");
+        String texto = String.valueOf(texto_introducido.getText());
+
+       Log.d("prueba", texto);
     }
 }
