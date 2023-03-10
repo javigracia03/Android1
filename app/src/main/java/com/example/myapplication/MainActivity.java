1ccbca66bd1359ea68b1c2a2e07523413137faa0
Javier Gracia <javiergracia@MacBook-Pro-de-Javier.local>
2023-03-10 21:42:38 +0100

    si pulsas botton sale Boton pulsado en la consola

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView texto_mostrado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //texto_mostrado = (TextView) findViewById(R.id.solucion);
+        //texto_mostrado.setText("HOLA SOY JAVI");
+    }
+
+    public void boton_pulsado(View quien){
+       Log.d("prueba", "EL BOTON SE HA PULSADO");
     }
 }
